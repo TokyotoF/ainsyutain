a5d63443bf43b6085b8fce7b10a591c5042883e7
TokyoF <koga0214@outlook.jp>
2023-09-15 15:41:48 +0900

    404ページ作成とヘッダーのインクルード

---
+++ js/footer.js
@@ -0,0 +1,1 @@
+document.write('<head><link rel="stylesheet" href="css/betastyle.css"><title>アインシュタイン公式ホームページ</title></head><body><div class="pbox"><div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div><div class="DivLink"><a class="you">index.htmlに戻る</a><a class ="Link" href="index.html"></a></div></div></body>')
