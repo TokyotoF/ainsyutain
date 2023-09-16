75d46aa2784c9d2fa46ca00771822566dfe335f6
TokyoF <koga0214@outlook.jp>
2023-09-16 16:20:51 +0900

    ついでにヘッダーもインクルード

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-document.write('<head><link rel="stylesheet" href="css/betastyle.css"><title>アインシュタイン公式ホームページ</title></head><body><div class="pbox"><div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div><div class="DivLink"><a class="you">index.htmlに戻る</a><a class ="Link" href="index.html"></a></div><div class="DivLink"><a class="you">404ページ</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain/hugahuga"></a></div></div></body>')
+document.write('<head><link rel="stylesheet" href="css/betastyle.css"><title>アインシュタイン公式ホームページ</title></head><body><div class="pbox"><div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div><div class="DivLink"><a class="you">index.htmlに戻る</a><a class ="Link" href="index.html"></a></div><div class="DivLink"><a class="you">404ページ</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain/404"></a></div></div></body>')
 //<div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div>
 
 //（削除済みの行）<p class="gaibunet"><font color="#00ff00">外部リンク</font></p>
@@ -13,6 +13,6 @@ document.write('<head><link rel="stylesheet" href="css/betastyle.css"><title>ア
 //</div>
 //<div class="DivLink">
 //<a class="you">404ページ</a>
-//<a class ="Link" href="https://github.com/TokyotoF/ainsyutain/hugahuga"></a>
+//<a class ="Link" href="https://tokyotof.github.io/ainsyutain/404"></a>
 //</div>
 //</div>
---
+++ js/header.js
@@ -0,0 +1,1 @@
+document.write('<p class="nameclass">【公式】アインシュタインのホームページ(開発中)</p>')
