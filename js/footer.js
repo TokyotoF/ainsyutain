53941faafffc0e79dde0c08c7ae3d3717ae76249
TokyoF <koga0214@outlook.jp>
2023-09-16 16:08:04 +0900

    githubでhttps:/github.com/TokyotoF/ainsyutain/404だとgithubの404ページに行ってしまう仕様に気づいたのでainsyutain/404からainsyutain/hugahugaに修正

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-document.write('<head><link rel="stylesheet" href="css/betastyle.css"><title>アインシュタイン公式ホームページ</title></head><body><div class="pbox"><div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div><div class="DivLink"><a class="you">index.htmlに戻る</a><a class ="Link" href="index.html"></a></div><div class="DivLink"><a class="you">404ページ</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain/404"></a></div></div></body>')
+document.write('<head><link rel="stylesheet" href="css/betastyle.css"><title>アインシュタイン公式ホームページ</title></head><body><div class="pbox"><div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div><div class="DivLink"><a class="you">index.htmlに戻る</a><a class ="Link" href="index.html"></a></div><div class="DivLink"><a class="you">404ページ</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain/hugahuga"></a></div></div></body>')
 //<div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div>
 
 //（削除済みの行）<p class="gaibunet"><font color="#00ff00">外部リンク</font></p>
@@ -10,5 +10,9 @@ document.write('<head><link rel="stylesheet" href="css/betastyle.css"><title>ア
 //<div class="DivLink">
 //<a class="you">2page.htmlに飛ぶ</a>
 //<a class ="Link" href="2page.html"></a>
+//</div>
+//<div class="DivLink">
+//<a class="you">404ページ</a>
+//<a class ="Link" href="https://github.com/TokyotoF/ainsyutain/hugahuga"></a>
 //</div>
 //</div>
