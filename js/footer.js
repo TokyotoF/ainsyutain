34eca27beaf0c4fbab8725cb66b28eb5059016de
TokyoF <koga0214@outlook.jp>
2023-09-23 22:02:38 +0900

    url修正

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-document.write('<head><link rel="stylesheet" href="css/betastyle.css"><title>アインシュタイン公式ホームページ</title></head><body><div class="pbox"><div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div><div class="DivLink"><a class="you">index.htmlに戻る</a><a class ="Link" href="index.html"></a></div><div class="DivLink"><a class="you">404ページ</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain/404"></a></div></div></body>')
+document.write('<head><link rel="stylesheet" href="css/betastyle.css"><title>アインシュタイン公式ホームページ</title></head><body><div class="pbox"><div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://tokyotof.github.io/ainsyutain/404"></a></div></div></body>')
 //<div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div>
 
 //（削除済みの行）<p class="gaibunet"><font color="#00ff00">外部リンク</font></p>
