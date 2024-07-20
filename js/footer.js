efbc36769792808fc4c282c787ce159036f2a886
TokyoF <koga0214@outlook.jp>
2024-07-20 17:24:30 +0900

    リンク削除

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,6 @@
-document.write('<div class="pbox"><div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div><div class="DivLink"><a class="you">吉田紘一の自己紹介</a><a class ="Link" href="2page.html"></a></div><div class="DivLink"><a class="you">404ページ</a><a class ="Link" href="404.html"></a></div><div class="DivLink"><a class="you">吉田紘一ゲーム研究所</a><a class ="Link" href="https://sites.google.com/d/1H1KA_LWhw2hAngWY2fwJgCX9MMhH1_-a/p/1BGTh7Qqk6WesIfQlMqUcghOoXhNTNc7M/edit"></a></div></div>')
+document.write(
+  '<div class="pbox"><div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div><div class="DivLink"><a class="you">吉田紘一の自己紹介</a><a class ="Link" href="2page.html"></a></div><div class="DivLink"><a class="you">404ページ</a><a class ="Link" href="404.html"></a></div></div>'
+);
 
 //<div class="pbox"><div class="DivLink"><a class="you">ソースコード(github)</a><a class ="Link" href="https://github.com/TokyotoF/ainsyutain"></a></div><div class="DivLink"><a class="you">2page.htmlに飛ぶ</a><a class ="Link" href="2page.html"></a></div><div class="DivLink"><a class="you">404ページ</a><a class ="Link" href="https://tokyotof.github.io/ainsyutain/404"></a></div></div>
 
@@ -15,9 +17,5 @@ document.write('<div class="pbox"><div class="DivLink"><a class="you">ソース�
 //<div class="DivLink">
 //<a class="you">404ページ</a>
 //<a class ="Link" href="404.html"></a>
-//</div>
-//<div class="DivLink">
-//<a class="you">吉田紘一ゲーム研究所</a>
-//<a class ="Link" href="https://sites.google.com/d/1H1KA_LWhw2hAngWY2fwJgCX9MMhH1_-a/p/1BGTh7Qqk6WesIfQlMqUcghOoXhNTNc7M/edit"></a>
 //</div>
 //</div>
--- js/header.js
+++ js/header.js
@@ -1,1 +1,3 @@
-document.write('<div class="name"><a class="name" href="https://tokyotof.github.io/ainsyutain/"><p class="nameclass">【公式】アインシュタインのホームページ(開発中)</p></a></div>')
+document.write(
+  '<div class="name"><a class="name" href="https://tokyotof.github.io/ainsyutain/"><p class="nameclass">【公式】アインシュタインのホームページ(開発中)</p></a></div>'
+);
